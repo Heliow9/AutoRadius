f649935017653c45110635e72a125c78d0e5ff0b
unknown <helio.paraiso@gmail.com>
2021-03-25 21:48:43 -0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,40 @@
 import Head from 'next/head'
+import { useState } from 'react'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+
+  const [topleft, setLeftRadius] = useState()
+  const [topRight, setRightRadius] = useState()
+  const [bottomleft, setLeftBRadius] = useState()
+  const [bottomRight, setRighttBRadius] = useState()
+  const [useColor, setBgColor] = useState();
+ 
+  const HandlerGetStyle = () =>{
+   
+  }
+
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+<div className={styles.container} style={{ backgroundColor: useColor ? `#${useColor}` : '#cecece' }} >
+      <h1>Trabalhando com Background e BorderRadius Dinâmicas</h1>
+    <div className={styles.BackgroundGroud}>
+      <label htmlFor="bgcolor">Adicione um código Hexadecimal da cor:</label>
+      <input name="bgcolor" className={styles.inputColor} type="text" placeholder="BackgroundColor" onChange={event => (setBgColor(event.target.value))} />
+    </div>
+      <button className={styles.geratebtn} onClick={HandlerGetStyle}>Gerar Código CSS</button>
+      <div id="Radius" className={styles.EditableRadius} style={{ borderTopLeftRadius: topleft ? parseInt(topleft) : 0, borderTopRightRadius: topRight ? parseInt(topRight) : 0, borderBottomLeftRadius: bottomleft ? parseInt(bottomleft) : 0, borderBottomRightRadius: bottomRight ? parseInt(bottomRight) : 0 }} >
+        <div className={styles.topInputs}>
+          <input type="number" onChange={event => (setLeftRadius(event.target.value))} />
+          <input type="number" onChange={event => (setRightRadius(event.target.value))} />
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+        <h3>Resul</h3>
+        <div className={styles.bottomInputs}>
+          <input type="number" onChange={event => (setLeftBRadius(event.target.value))} />
+          <input type="number" onChange={event => (setRighttBRadius(event.target.value))} />
+        </div>
+      </div>
     </div>
   )
 }
