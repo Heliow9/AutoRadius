bf22b6cbe90f531e52d9ebe54e442144d921f566
unknown <helio.paraiso@gmail.com>
2021-03-26 21:45:28 -0300

    Add css function

--- pages/index.js
+++ pages/index.js
@@ -9,27 +9,39 @@ export default function Home() {
   const [bottomleft, setLeftBRadius] = useState()
   const [bottomRight, setRighttBRadius] = useState()
   const [useColor, setBgColor] = useState();
- 
-  const HandlerGetStyle = () =>{
-   
+  const [styled, setCss] = useState([]);
+
+  const HandlerGetStyle = () => {
+    let data = new Object;
+    const css = document.getElementById('Radius');
+    const style = getComputedStyle(css)
+    data.width = style.width;
+    data.heigth = style.height;
+    data.border = style.border;
+    data.borderTopLeftRadius = style.borderTopLeftRadius;
+    data.borderTopRightRadius = style.borderTopRightRadius;
+    data.borderBottomLeftRadius = style.borderBottomLeftRadius;
+    data.borderBottomRightRadius = style.borderBottomRightRadius;
+
   }
 
 
   return (
-<div className={styles.container} style={{ backgroundColor: useColor ? `#${useColor}` : '#cecece' }} >
+    <div className={styles.container} style={{ backgroundColor: useColor ? `#${useColor}` : '#cecece' }} >
       <h1>Trabalhando com Background e BorderRadius Dinâmicas</h1>
-    <div className={styles.BackgroundGroud}>
-      <label htmlFor="bgcolor">Adicione um código Hexadecimal da cor:</label>
-      <input name="bgcolor" className={styles.inputColor} type="text" placeholder="BackgroundColor" onChange={event => (setBgColor(event.target.value))} />
-    </div>
+      <div className={styles.BackgroundGroud}>
+        <label htmlFor="bgcolor">Adicione um código Hexadecimal da cor:</label>
+        <input name="bgcolor" className={styles.inputColor} type="text" placeholder="BackgroundColor" onChange={event => (setBgColor(event.target.value))} />
+      </div>
       <button className={styles.geratebtn} onClick={HandlerGetStyle}>Gerar Código CSS</button>
       <div id="Radius" className={styles.EditableRadius} style={{ borderTopLeftRadius: topleft ? parseInt(topleft) : 0, borderTopRightRadius: topRight ? parseInt(topRight) : 0, borderBottomLeftRadius: bottomleft ? parseInt(bottomleft) : 0, borderBottomRightRadius: bottomRight ? parseInt(bottomRight) : 0 }} >
         <div className={styles.topInputs}>
           <input type="number" onChange={event => (setLeftRadius(event.target.value))} />
           <input type="number" onChange={event => (setRightRadius(event.target.value))} />
-
         </div>
-        <h3>Resul</h3>
+        <h3>{styled.map((item, key) => (
+          console.log(item.width)
+        ))}</h3>
         <div className={styles.bottomInputs}>
           <input type="number" onChange={event => (setLeftBRadius(event.target.value))} />
           <input type="number" onChange={event => (setRighttBRadius(event.target.value))} />
